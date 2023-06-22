1eb3d693f9812591e900fd3757c63a4fef39c070
n-kawauchi <n.kawauchi@aist.go.jp>
2023-06-22 10:48:41 +0900

    bug fixed.

--- custom/build.py
+++ custom/build.py
@@ -123,28 +123,28 @@ class BuildIDL(Command):
         #    self.compile_one_idl(f)
 
         #../examples/SimpleService
-        #self.mkpath(self.examples_dir)
+        ##self.mkpath(self.examples_dir)
             #self.idl_dir = os.path.join(self.idl_dir, self.idl_path)
-        #self.idl_dir = os.path.join(self.idl_dir, 'OpenRTM_aist/RTM_IDL')
-        #idl_target_dir = os.path.join(self.idl_src_dir, '../examples/SimpleService')
-        idl_target_dir = os.path.join(self.examples_dir, 'SimpleService')
-        idl_files = [os.path.join(idl_target_dir, f)
-                     for f in os.listdir(idl_target_dir)
-                     if os.path.splitext(f)[1] == '.idl']
+        ##self.idl_dir = os.path.join(self.idl_dir, 'OpenRTM_aist/RTM_IDL')
+        ##idl_target_dir = os.path.join(self.idl_src_dir, '../examples/SimpleService')
+        #idl_target_dir = os.path.join(self.examples_dir, 'SimpleService')
+        #idl_files = [os.path.join(idl_target_dir, f)
+        #             for f in os.listdir(idl_target_dir)
+        #             if os.path.splitext(f)[1] == '.idl']
         #pkg_param = '-Wbstubs=OpenRTM_aist.examples.SimpleService'
-        pkg_param = '-Wbpackages=OpenRTM_aist.examples.SimpleService'
-        for f in idl_files:
-            self.compile_example_idl(f, pkg_param)
+        #pkg_param = '-Wbpackages=OpenRTM_aist.examples.SimpleService'
+        #for f in idl_files:
+        #    self.compile_example_idl(f, pkg_param)
 
         #../examples/AutoTest
-        idl_target_dir = os.path.join(self.examples_dir, 'AutoTest')
-        idl_files = [os.path.join(idl_target_dir, f)
-                     for f in os.listdir(idl_target_dir)
-                     if os.path.splitext(f)[1] == '.idl']
-        #pkg_param = '-Wbstubs=OpenRTM_aist.examples.AutoTest'
-        pkg_param = '-Wbpackages=OpenRTM_aist.examples.AutoTest'
-        for f in idl_files:
-            self.compile_example_idl(f, pkg_param)
+        #idl_target_dir = os.path.join(self.examples_dir, 'AutoTest')
+        #idl_files = [os.path.join(idl_target_dir, f)
+        #             for f in os.listdir(idl_target_dir)
+        #             if os.path.splitext(f)[1] == '.idl']
+        ##pkg_param = '-Wbstubs=OpenRTM_aist.examples.AutoTest'
+        #pkg_param = '-Wbpackages=OpenRTM_aist.examples.AutoTest'
+        #for f in idl_files:
+        #    self.compile_example_idl(f, pkg_param)
 
     def move_stubs(self):
         stub_dest = os.path.join(self.build_lib, 'OpenRTM_aist', 'RTM_IDL')
